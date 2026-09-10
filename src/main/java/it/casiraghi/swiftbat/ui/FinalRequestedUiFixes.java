ee1fcf48a4aacc0a4514cc02928162a8de175f53
CoachCasira <146272372+CoachCasira@users.noreply.github.com>
2026-09-10 20:03:48 +0200

    add final requested UI fixes

---
+++ src/main/java/it/casiraghi/swiftbat/ui/FinalRequestedUiFixes.java
@@ -0,0 +1,8 @@
+package it.casiraghi.swiftbat.ui;
+
+import javafx.scene.Parent;
+
+public final class FinalRequestedUiFixes {
+    private FinalRequestedUiFixes() { }
+    public static void install(Parent root) { }
+}
